695026efc5aab4d36c15a14837faff790b338da3
Frank Poon <frankgpoon@gmail.com>
2020-01-23 02:37:54 -0800

    Add error handling and status

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { parseUserMessage } from './commands.js';
 
 process.env.DEBUG_LEVEL = 'info';
 
+import {token} from './token.js'
+process.env.TOKEN = token;
+
 const TOKEN = process.env.TOKEN;
 const DEBUG_LEVEL = process.env.DEBUG_LEVEL;
 
 // create client
 const client = new Discord.Client();
 const logger = winston.createLogger({
-  level: process.env.DEBUG_LEVEL,
+  level: DEBUG_LEVEL,
   transports: [new winston.transports.Console()]
 });
 
@@ -19,6 +22,8 @@ logger.info('Starting up Adobot');
 // logging stuff when adobot signs in
 client.on('ready', () => {
   logger.info(`Logged in as ${client.user.tag}!`);
+  logger.info(`Setting up status`);
+  client.user.setActivity('Tyler feed', { type: 'WATCHING' })
 });
 
 // parse messages
@@ -30,5 +35,20 @@ client.on('message', async msg => {
 
 });
 
+client.on('error', (err) => {
+  logger.error(err);
+  client.destroy();
+  process.exit();
+})
+
 // login with given token
 client.login(TOKEN);
+
+
+
+process.on('SIGINT', () => {
+  logger.info('Adobot is shutting down');
+
+  client.destroy();
+  process.exit();
+});
