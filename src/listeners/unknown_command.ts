58973dab69ad531efbfd3059e6781ba4ae8af01c
Frank Poon <frankgpoon@gmail.com>
2022-04-21 19:38:42 -0400

    Add unknown command listener

---
+++ src/listeners/unknown_command.ts
@@ -0,0 +1,23 @@
+import { Listener, UnknownCommandPayload } from '@sapphire/framework';
+import { loggers } from 'winston';
+
+const logger = loggers.get('global_logger');
+
+export class UnknownCommandListener extends Listener {
+  constructor(context: Listener.Context, options: Listener.Options) {
+    super(context, {
+      ... options,
+      event: 'unknownCommand'
+    });
+    
+  }
+
+  
+  public run({ message }: UnknownCommandPayload) {
+    logger.info(`Sent response after receiving an unknown message`);
+    logger.verbose(`Response was to ${message.author.tag} in ${message.channel.type} ` +
+      `channel ${message.channel.id}`);
+    
+    return message.reply(`OK ${message.author} I do not even know what you are saying (type ~help for help)`);
+  }
+}
